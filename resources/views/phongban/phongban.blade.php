753e859248e35e343c2495ce06e8a8303ad47c34
sieugadev <thachchanhtha@gmail.com>
2022-11-18 21:48:16 +0700

    bye bye ngu ngon

--- resources/views/phongban/phongban.blade.php
+++ resources/views/phongban/phongban.blade.php
@@ -4,6 +4,7 @@
              sao lại không có gì chứ
              tôi nhận được rồi, giờ bạn ổn không?
              tôi ở đây rất ổn, tôi sẽ gửi thêm file cho bạn
+             tôi biết bạn buồn ngủ rồi, tôi có gửi thêm file đó
 
        @endsection
 
---
+++ resources/views/thongtinquantrong/code_html.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang=eu>
+    <head>
+        <meta charset="UTF-8">
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <title>MVC</title>
+
+    </head>
+ <body>
+       
+ </body>
+</html>
